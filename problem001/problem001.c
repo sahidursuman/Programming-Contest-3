d71c6bcef3481c6cc4107e446ce6da96460c0c4b
maze1230 <labyrinth1230@gmail.com>
2012-12-22 19:51:29 +0900

    SublimeText2

--- problem001/problem001.c
+++ problem001/problem001.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 
 int main(void){
-	int sum=0;//答えを入力
+	int ans=0;//答えを入力
 	int loop_i;//ループ用
 	
 	for(loop_i=0; loop_i<1000;loop_i++){
 		if(loop_i%3==0 || loop_i%5==0)
-			sum += loop_i;//もしloop_iが3か5の倍数なら、sumに足す
+			ans += loop_i;//もしloop_iが3か5の倍数なら、ansに足す
 	}
 	
-	printf("answer :%d\n",sum);//答えを入力
+	printf("answer :%d\n",ans);//答えを入力
 	
 	return 0;
 }
